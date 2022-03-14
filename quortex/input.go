ef0ff5316d262303252f03a3eb5129a910b10d3d
Thierry <thierry@quortex.io>
2022-03-14 22:52:19 +0100

    Upd nested input streams.

--- quortex/input.go
+++ quortex/input.go
@@ -84,6 +84,27 @@ func (c *Client) CreateInput(poolName string, input Input) (*Input, error) {
 
 // UpdateInput - Updates a input
 func (c *Client) UpdateInput(poolName string, inputName string, input Input) (*Input, error) {
+
+	// First update streams
+	for _, stream := range input.Streams {
+		rb, err := json.Marshal(stream)
+		if err != nil {
+			return nil, err
+		}
+
+		log.Println(string(rb))
+		req, err := http.NewRequest("PUT", fmt.Sprintf("%s/1.0/pools/%s/inputs/%s/streams/%s", c.HostURL, poolName, inputName, stream.Uuid), strings.NewReader(string(rb)))
+		if err != nil {
+			return nil, err
+		}
+
+		_, err = c.doRequest(req)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// Second update input
 	rb, err := json.Marshal(input)
 	if err != nil {
 		return nil, err
