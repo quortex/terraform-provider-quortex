1f1fa43fe83a098354f4043e72ab130ebb05de8c
Thierry Trolez <54419332+Thierry-35@users.noreply.github.com>
2023-06-14 09:10:28 +0200

    Add ksm & mdrm drm vendors. (#20)
    Co-authored-by: Thierry Trolez <thierry@quortex.io>

--- quortex/model_drm_merchant.go
+++ quortex/model_drm_merchant.go
@@ -18,6 +18,21 @@ type Irdeto struct {
 	Password     string `json:"password,omitempty"`
 }
 
+// Ksm -
+type Ksm struct {
+	MerchantName string `json:"merchant_name,omitempty"`
+	DrmServer    string `json:"drm_server,omitempty"`
+}
+
+// Mdrm -
+type Mdrm struct {
+	MerchantName string `json:"merchant_name,omitempty"`
+	AuthServer   string `json:"auth_server,omitempty"`
+	ClientId     string `json:"client_id,omitempty"`
+	ClientSecret string `json:"client_secret,omitempty"`
+	DrmServer    string `json:"drm_server,omitempty"`
+}
+
 // DrmMerchant -
 type DrmMerchant struct {
 	Uuid     string    `json:"uuid,omitempty"`
@@ -25,4 +40,6 @@ type DrmMerchant struct {
 	Type     string    `json:"type,omitempty"`
 	Castlabs *Castlabs `json:"castlabs,omitempty"`
 	Irdeto   *Irdeto   `json:"irdeto,omitempty"`
+	Ksm      *Ksm      `json:"ksm,omitempty"`
+	Mdrm     *Mdrm     `json:"mdrm,omitempty"`
 }
--- quortex/resource_ott_drm_merchant.go
+++ quortex/resource_ott_drm_merchant.go
@@ -80,6 +80,48 @@ func resourceOttDrmMerchant() *schema.Resource {
 					},
 				},
 			},
+			"ksm": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				MinItems: 0,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"drm_server": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
+			"mdrm": {
+				Type:     schema.TypeList,
+				Optional: true,
+				Computed: true,
+				MinItems: 0,
+				MaxItems: 1,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"auth_server": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"client_id": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"client_secret": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"drm_server": {
+							Type:     schema.TypeString,
+							Required: true,
+						},
+					},
+				},
+			},
 		},
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
@@ -91,6 +133,8 @@ func marshallModelDrmMerchant(d *schema.ResourceData) (*DrmMerchant, error) {
 
 	castlabs := d.Get("castlabs").([]interface{})
 	irdetos := d.Get("irdeto").([]interface{})
+	ksms := d.Get("ksm").([]interface{})
+	mdrms := d.Get("mdrm").([]interface{})
 	ve := DrmMerchant{
 		Name: d.Get("name").(string),
 	}
@@ -122,6 +166,31 @@ func marshallModelDrmMerchant(d *schema.ResourceData) (*DrmMerchant, error) {
 			}
 			ve.Irdeto = &irde
 			ve.Type = "irdeto"
+		}
+	}
+
+	for _, ksm := range ksms {
+		if ksm != nil {
+			ks := ksm.(map[string]interface{})
+			k := Ksm{
+				DrmServer: ks["drm_server"].(string),
+			}
+			ve.Ksm = &k
+			ve.Type = "ksm"
+		}
+	}
+
+	for _, mdrm := range mdrms {
+		if mdrm != nil {
+			mdr := mdrm.(map[string]interface{})
+			md := Mdrm{
+				AuthServer:   mdr["auth_server"].(string),
+				ClientId:     mdr["client_id"].(string),
+				ClientSecret: mdr["client_secret"].(string),
+				DrmServer:    mdr["drm_server"].(string),
+			}
+			ve.Mdrm = &md
+			ve.Type = "mdrm"
 		}
 	}
 
@@ -173,6 +242,16 @@ func resourceDrmMerchantRead(ctx context.Context, d *schema.ResourceData, m inte
 
 	irdeto := flattenDrmMerchantIrdeto(drmmerchant.Irdeto)
 	if err := d.Set("irdeto", irdeto); err != nil {
+		return diag.FromErr(err)
+	}
+
+	ksm := flattenDrmMerchantKsm(drmmerchant.Ksm)
+	if err := d.Set("ksm", ksm); err != nil {
+		return diag.FromErr(err)
+	}
+
+	mdrm := flattenDrmMerchantMdrm(drmmerchant.Mdrm)
+	if err := d.Set("mdrm", mdrm); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -241,6 +320,27 @@ func flattenDrmMerchantIrdeto(irdeto *Irdeto) []interface{} {
 		c["drm_server"] = irdeto.DrmServer
 		c["username"] = irdeto.Username
 		c["password"] = irdeto.Password
+	}
+	return []interface{}{c}
+}
+
+func flattenDrmMerchantKsm(ksm *Ksm) []interface{} {
+
+	c := make(map[string]interface{})
+	if ksm != nil {
+		c["drm_server"] = ksm.DrmServer
+	}
+	return []interface{}{c}
+}
+
+func flattenDrmMerchantMdrm(mdrm *Mdrm) []interface{} {
+
+	c := make(map[string]interface{})
+	if mdrm != nil {
+		c["auth_server"] = mdrm.AuthServer
+		c["client_id"] = mdrm.ClientId
+		c["client_secret"] = mdrm.ClientSecret
+		c["drm_server"] = mdrm.DrmServer
 	}
 	return []interface{}{c}
 }
