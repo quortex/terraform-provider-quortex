1a92f04ecc0817da51c408ed11749ad62358c387
Vincent Marguerie <24724195+vincentmrg@users.noreply.github.com>
2024-04-02 18:30:36 +0200

    Add processing_type attribute for quortex_ott_pool resource

--- examples/development/pool.tf
+++ examples/development/pool.tf
@@ -1,6 +1,14 @@
 resource "quortex_ott_pool" "my_pool" {
   name                = "my_pool"
   streaming_countries = ["FRA"]
-  input_region        = "ireland"
+  input_region        = "france"
   published           = true
+}
+
+resource "quortex_ott_pool" "my_pool_advanced" {
+  name                = "my_pool_advanced"
+  streaming_countries = ["FRA"]
+  input_region        = "france"
+  published           = true
+  processing_type     = "advanced"
 }
